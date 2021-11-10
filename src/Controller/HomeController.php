973d7c2214d0ecf755e275b9f2fc2696c2943794
Baptiste BOUDAUD <baptiste.boudaud85@gmail.com>
2021-11-10 10:39:35 +0100

    parametrage OK

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -14,6 +14,6 @@ class HomeController extends AbstractController
      */
     public function index(): Response
     {
-        return $this->render('Hackathon/pageAccueil.html.twig');
+        return $this->render('pageAccueil.html.twig');
     }
 }
