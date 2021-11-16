67c29806623843f246cf5ce2a8b0fbae9b9fe44b
Erwan Réto <erwan.reto85@gmail.com>
2021-11-16 14:16:18 +0100

    Merge branch 'dev' of https://github.com/R1ReTo/-Hackathon

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,27 +2,44 @@
 
 namespace App\Controller;
 
+use App\Entity\Hackathon;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Entity\Hackathon;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Serializer;
 
 class HomeController extends AbstractController
 {
     /**
-     * @Route("/", name="home")
+     * @Route("/listeHackathon", name="listeHackathon")
      */
     public function index(): Response
     {
-        return $this->render('pageAccueil.html.twig');
+        $repository = $this->getDoctrine()->getRepository(Hackathon::class);
+        $products = $repository->findAll();
+        return $this->render('listeHackathon.html.twig', ['lesHackathons' => $products]);       
     }
+
+
+    /**
+     * @Route("/getHackathon", name="getHackathon",methods="GET")
+     */
+    public function getHackathon(): Response
+    {
+        $serializer = $this->get('serializer');
+        $repository = $this->getDoctrine()->getRepository(Hackathon::class);
+        $products = $repository->findAll();
+        $json = $serializer->serialize($products, 'json');
+        return new Response($json); 
+    }
+
 
     /**
      * @Route("/", name="home")
      */
-    public function detailsHackathon(): Response
+    public function accueil(): Response
     {
-        return $this->render('detailsHackathon.html.twig');
+        return $this->render('pageAccueil.html.twig');
     }
-
 }
