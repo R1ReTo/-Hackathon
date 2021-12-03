84e34439d386c3d100cb4d685bf0bdbf1ab362af
Erwan Réto <erwan.reto85@gmail.com>
2021-12-03 10:57:32 +0100

    api liste atelier

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -67,4 +67,28 @@ class HomeController extends AbstractController
     }
     */
 
+    /**
+     * @Route("/api/listeAtelier/{idhackathon}", name="listeAtelier")
+     */
+    public function listeAtelier($idhackathon):Response
+    {
+        $serializer = $this->get('serializer');
+        $repository = $this->getDoctrine()->getRepository(Evenement::class);
+        $products = $repository->findBy(['idhackathon' => $idhackathon]);
+        $json = $serializer->serialize($products, 'json');
+        return new Response($json); 
+    }
+    
+    /**
+     * @Route("/api/detailAtelier/{idevenement}", name="listeAtelier")
+     */
+    public function detailAtelier($idevenement):Response
+    {
+        $serializer = $this->get('serializer');
+        $repository = $this->getDoctrine()->getRepository(Initiation::class);
+        $products = $repository->findBy(['idevenement' => $idevenement]);
+        $json = $serializer->serialize($products, 'json');
+        return new Response($json); 
+    }
+
 }
